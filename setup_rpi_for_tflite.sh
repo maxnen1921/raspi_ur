05233ae146174e57ed30f9a0aa14bc932f77a8b1
maxnen1921 <maxim.nenahof@kindustrii.de>
2022-02-21 01:34:37 +0100

    added environment setup instructions

---
+++ setup_rpi_for_tflite.sh
@@ -0,0 +1,34 @@
+# Show your Raspberry Pi OS version.
+cat /etc/os-release
+
+# Update packages on your Raspberry Pi OS.
+sudo apt-get update
+
+# Check your Python version. You should have Python 3.7 or later.
+python3 --version
+
+# Install virtualenv and upgrade pip.
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user virtualenv
+
+# Create a Python virtual environment for the TFLite samples (optional but strongly recommended)
+python3 -m venv /tflite
+
+# Run this command whenever you open a new Terminal window/tab to activate the environment.
+source /tflite/bin/activate
+
+# Clone the TensorFlow example repository with the TFLite Raspberry Pi samples.
+# git clone https://github.com/tensorflow/examples.git
+#cd examples/lite/examples/object_detection/raspberry_pi
+
+# Install dependencies required by the sample
+sh setup.sh
+
+# Run the object detection sample
+# python detect.py
+
+####
+# If you see an error running the sample:
+# ImportError: libcblas.so.3: cannot open shared object file: No such file or directory
+# you can fix it by installing an OpenCV dependency that is missing on your Raspberry Pi.
+sudo apt-get install libatlas-base-dev
