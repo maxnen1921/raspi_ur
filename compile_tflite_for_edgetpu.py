c6b1c9e441cd9d3e15ae7b629e799cdf8aa8dbbd
maxnen1921 <maxim.nenahof@kindustrii.de>
2022-02-23 14:42:25 +0100

    edgetpu compile script fixed

--- compile_tflite_for_edgetpu.py
+++ compile_tflite_for_edgetpu.py
@@ -1,9 +1,19 @@
+from tflite_support.metadata_writers import object_detector
+from tflite_support.metadata_writers import writer_utils
 from tflite_support import metadata
 
-populator_dst = metadata.MetadataPopulator.with_model_file('models_test/duplo_efficientdet_lite0_edgetpu.tflite')
+ObjectDetectorWriter = object_detector.MetadataWriter
+_MODEL_PATH = "/home/maxim/PycharmProjects/tflite/raspi_v1/models/duplo_efficientdet_lite0_edgetpu.tflite"
+_LABEL_FILE = "/home/maxim/PycharmProjects/tflite/raspi_v1/models/label.txt"
+_SAVE_TO_PATH = "/home/maxim/PycharmProjects/tflite/raspi_v1/models/duplo_efficientdet_lite0_edgetpu_meta.tflite"
 
-with open('models_test/duplo_efficientdet_lite0_edgetpu.tflite', 'rb') as f:
-  populator_dst.load_metadata_and_associated_files(f.read())
+writer = ObjectDetectorWriter.create_for_inference(
+    writer_utils.load_file(_MODEL_PATH), [127.5], [127.5], [_LABEL_FILE])
+writer_utils.save_file(writer.populate(), _SAVE_TO_PATH)
 
-populator_dst.populate()
-updated_model_buf = populator_dst.get_model_buffer()
+# Verify the populated metadata and associated files.
+displayer = metadata.MetadataDisplayer.with_model_file(_SAVE_TO_PATH)
+print("Metadata populated:")
+print(displayer.get_metadata_json())
+print("Associated file(s) populated:")
+print(displayer.get_packed_associated_file_list())
